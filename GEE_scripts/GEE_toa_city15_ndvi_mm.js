912f5eb80bb04803894ce01769bec50fc5a7355d
ogletrees <ogletrees@users.noreply.github.com>
2018-01-03 17:32:24 -0500

    Create GEE_toa_city15_ndvi_mm.js

---
+++ GEE_scripts/GEE_toa_city15_ndvi_mm.js
@@ -0,0 +1,49 @@
+// LandSat data, TOA dataset. Filter to 2015 May - Sept
+var landsat = ee.ImageCollection("LANDSAT/LC8_L1T_TOA")
+    .filterDate('2015-05-01', '2016-09-30')
+// City bounds
+var cbg = ee.FeatureCollection("users/ssogletree/cities")
+
+// calculate NDVI, mapping over image collection
+var ndvi = landsat.map(function(image) {
+  var result = image.normalizedDifference(['B5', 'B4']).rename("ndvi")
+  return image.addBands(result);
+})
+
+// Make a "greenest" pixel composite.
+var greenest = ndvi.qualityMosaic('ndvi')
+
+// Get just the NDVI value band
+var val_NDVI = greenest.select('ndvi')
+
+// Get elevation to mask 0
+var elevation = ee.Image("USGS/SRTMGL1_003") // elevation data
+
+  
+// apply mask    
+var wmask = val_NDVI.mask(elevation.neq(0))
+
+// Map over feature collection, get the mean NDVI
+var meancbgndvi = wmask.reduceRegions({
+  collection: cbg,
+  reducer: ee.Reducer.mean().setOutputs(['mean_ndvi']).combine({
+		reducer2: ee.Reducer.median().setOutputs(['median_ndvi']),
+    sharedInputs: true
+  }),
+  scale: 30 // 30 meters
+});
+
+// Print the first to check, just the first 6 elements
+// print(meancityndvi.toList(6))
+
+
+/* export to google drive */
+ Export.table.toDrive({
+  collection: meancbgndvi, 
+  description: 'cbg15_mean_ndvi_20180101', 
+  folder: 'RemoteSensingWork', 
+//fileNamePrefix: , 
+  fileFormat: 'CSV'
+  }); 
+
+print('done')
