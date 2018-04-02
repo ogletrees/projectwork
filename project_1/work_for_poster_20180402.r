e7bbcf9e86fe90c8799181787d0f5220b31ea17d
ogletrees <scott.ogletree@gmail.com>
2018-04-02 15:19:57 -0400

    add script for poster work

---
+++ project_1/work_for_poster_20180402.r
@@ -0,0 +1,34 @@
+# 
+#
+# Work through modeling for project 1 that I want to use for the research poster on April 6, 2018
+
+library(tidyverse)
+library(lme4)
+library(multilevel)
+library(jtools)
+
+
+# Data ---------------------------------------------
+
+
+
+
+# Model 1 - no city context ---------------------------------------------
+
+
+
+
+
+# Intercept only model ---------------------------------------------
+
+
+
+
+
+# Model with level 1 vars ---------------------------------------------
+
+
+
+
+
+# Model with level 1 & 2 vars ---------------------------------------------
